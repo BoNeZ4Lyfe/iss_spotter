13e0c0463141326817a692018584703c538c985d
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2021-12-07 07:18:42 -0500

    iss spotter IV

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-const exampleCoords = { latitude: '43.8770', longitude: '-79.2262' };
+/** 
+ * Input: 
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns: 
+ *   undefined
+ * Sideffect: 
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
 
-fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+const printPassTimes = function(passTimes) {
+
+  for (const pass of passTimes) {
+
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
 
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log('It worked! Returned flyover times:' , passTimes);
+  printPassTimes(passTimes);
 });
